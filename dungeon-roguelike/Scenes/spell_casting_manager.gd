0937357577db8ae8e4b1f1d115e2da66e8e445b5
PranavAlla <Pranavalla@gmail.com>
2026-05-28 16:43:24 -0400

    Setup spell circle UI

--- dungeon-roguelike/Scenes/spell_casting_manager.gd
+++ dungeon-roguelike/Scenes/spell_casting_manager.gd
@@ -4,20 +4,9 @@ var slots: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func position_slots_on_circle(num_slots: int, radius: float):
-	for i in num_slots:
-		var slot = sigil_slot.instantiate()
-		add_child(slot)
-		slots.append(slot)
-		
-		var angle = (TAU / num_slots) * i
-		var slot_pos = Vector2(cos(angle), sin(angle)) * radius
-		slots[i].position = slot_pos
-		slots[i].metadata_angle = angle
