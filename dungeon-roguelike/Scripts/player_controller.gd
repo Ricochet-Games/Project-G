c213e0b327b3c33d4b1e142a1c64af7949b46713
Toxin4426 <Toxin4426@gmail.com>
2026-06-01 14:41:08 -0400

    rotate player to face movement dir

--- dungeon-roguelike/Scripts/player_controller.gd
+++ dungeon-roguelike/Scripts/player_controller.gd
@@ -16,10 +16,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
+		var target_rotation = atan2(direction.x, direction.z)
+		rotation.y = target_rotation + deg_to_rad(-90)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
