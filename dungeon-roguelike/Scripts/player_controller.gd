e7101b04384397c4f1c73eb5ebab5282d6e750f7
Toxin4426 <Toxin4426@gmail.com>
2026-06-01 14:26:55 -0400

    Player is no longer on an angle / rotated in code

--- dungeon-roguelike/Scripts/player_controller.gd
+++ dungeon-roguelike/Scripts/player_controller.gd
@@ -3,19 +3,19 @@ extends CharacterBody3D
 @export var speed: float = 5.0
 
 @export var camera: Camera3D
-@export var camera_follow_speed := 0.1
+@export var camera_follow_speed: float = 0.1
 
-func _physics_process(delta: float) -> void:	
-	
+@export var player_rotation_speed: float = 0.1
+
+func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
-	input_dir = input_dir.rotated(deg_to_rad(-45)) 
-	
+
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
@@ -26,7 +26,7 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	move_camera()
-	
-	
+
+
 func move_camera():
 	$Camera_Controller.position = lerp($Camera_Controller.position, position, camera_follow_speed)
