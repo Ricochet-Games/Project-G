fbfaabcfc163f9e8ea7413a21c1b85cbcaa2e105
Toxin4426 <Toxin4426@gmail.com>
2026-06-08 16:59:51 -0400

    create health component

---
+++ dungeon-roguelike/Assets/health_component.gd
@@ -0,0 +1,39 @@
+extends Node
+class_name Health_Component
+
+
+signal damaged(amount: int)
+signal healed(amount: int)
+signal health_changed(amount: int)
+signal died
+
+
+@export var max_health : int = 100
+
+@export var current_health : int
+
+func _ready() -> void:
+	current_health = max_health
+
+
+func take_damage(amount: int) -> void:
+	if current_health <= 0:
+		return
+
+	current_health = max(current_health - amount, 0)
+	damaged.emit(amount)
+	health_changed.emit(amount)
+	
+	if(current_health <= 0):
+		died.emit()
+
+func heal(amount: int) -> void:
+	if(current_health <= 0):
+		return
+	
+	current_health = min(current_health + amount, max_health)
+	healed.emit(amount)
+	health_changed.emit(current_health)
+	
+func is_alive() -> bool:
+	return current_health > 0
