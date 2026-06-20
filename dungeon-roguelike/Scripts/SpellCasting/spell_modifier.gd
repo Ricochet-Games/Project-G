c107af3bb74a4ab7cfb1718ce07a88eef4ed0da2
PranavAlla <Pranavalla@gmail.com>
2026-06-20 17:02:14 -0400

    Saving work, functional drag and drop UI

--- dungeon-roguelike/Scripts/SpellCasting/spell_modifier.gd
+++ dungeon-roguelike/Scripts/SpellCasting/spell_modifier.gd
@@ -1,5 +1,4 @@
-# Goes on the player, creates the projectiles for spells
-extends Node
+extends TextureRect
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+	var data = 1
+	var prev = TextureRect.new()
+	prev.texture = texture
+	set_drag_preview(prev)
+	
+	return self
