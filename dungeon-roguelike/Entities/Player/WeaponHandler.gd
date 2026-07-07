45b4e8282570f2dbaa14424411bdba835c6fa1b2
Toxin4426 <Toxin4426@gmail.com>
2026-07-07 18:28:43 -0400

    add offhand getter

--- dungeon-roguelike/Entities/Player/WeaponHandler.gd
+++ dungeon-roguelike/Entities/Player/WeaponHandler.gd
@@ -3,8 +3,16 @@ class_name WeaponHandler
 
 @export var equipped_weapon : WeaponData
 
+@export var equipped_offhand_weapon : WeaponData
+
 func get_weapon() -> WeaponData:
 	return equipped_weapon
 
 func equip_weapon(new_weapon : WeaponData) -> void:
+	equipped_weapon = new_weapon
+
+func get_offhand_weapon() -> WeaponData:
+	return equipped_weapon
+
+func equip_offhand_weapon(new_weapon : WeaponData) -> void:
 	equipped_weapon = new_weapon
