203f18c67b8b6540bc4da36fd93c1d6cfeaea44f
Toxin4426 <Toxin4426@gmail.com>
2026-07-09 09:47:19 -0400

    use attack skill

--- dungeon-roguelike/Entities/Player/attack_componenet.gd
+++ dungeon-roguelike/Entities/Player/attack_componenet.gd
@@ -3,6 +3,7 @@ class_name AttackComponent
 
 @export var weapon_handler : WeaponHandler
 @export var stamina_component : StaminaComponent
+@export var mana_component : ManaComponent
 var combo := 0
 var can_combo := false
 @export var combo_timer : Timer
@@ -51,10 +52,10 @@ func attack() -> void:
 
 	if stamina_component.current_stamina == 0:
 		return
+	if mana_component.current_mana < current_attack.mana_cost:
+		return
 		
 	perform_attack(current_attack)
-
-	
 
 @warning_ignore("shadowed_variable")
 func perform_attack(attack : AttackData) -> void:
@@ -65,7 +66,6 @@ func perform_attack(attack : AttackData) -> void:
 		hitboxes.append(hitbox)
 		hitbox.damage = attack.damage
 	
-	print(attack.resource_path + "  " + var_to_str(combo))
 	can_combo = false
 	
 	attack_state = AttackState.WINDING_UP
@@ -83,7 +83,7 @@ func perform_attack(attack : AttackData) -> void:
 	
 	hitboxes.clear()
 	attack_state = AttackState.RECOVERING
-	attacked.emit(attack.stamina_cost)
+	attacked.emit(attack.stamina_cost, attack.mana_cost)
 	
 	await get_tree().create_timer(attack.recovery).timeout
 	
@@ -91,4 +91,49 @@ func perform_attack(attack : AttackData) -> void:
 	can_combo = true
 	combo_timer.start(attack.combo_window)
 	combo += 1
+
+func attack_skill() -> void:
+	if not attack_state == AttackState.IDLE:
+		return
+
+	var weapon : WeaponData = weapon_handler.get_weapon()
+
+	if weapon == null:
+		return
 	
+	var current_attack_skill : AttackSkillData = weapon.attack_skills[0]
+
+	if current_attack_skill.mana_cost > 0 and stamina_component.current_stamina == 0:
+		return
+	if mana_component.current_mana < current_attack_skill.mana_cost:
+		return
+		
+	perform_attack_skill(current_attack_skill)
+@warning_ignore("shadowed_variable")
+func perform_attack_skill(attack_skill: AttackSkillData) -> void:
+	for packed_scene in attack_skill.hitbox_scenes:
+		var hitbox : Hitbox = packed_scene.instantiate()
+		attack_pivot.add_child(hitbox)
+		hitboxes.append(hitbox)
+		hitbox.damage = attack_skill.damage
+	
+	attack_state = AttackState.WINDING_UP
+	await get_tree().create_timer(attack_skill.windup).timeout
+	
+	for hitbox in hitboxes:
+		hitbox.enable_hitbox()
+	
+	attack_state = AttackState.ATTACKING
+	await get_tree().create_timer(attack_skill.active_time).timeout
+	
+	for hitbox in hitboxes:
+		hitbox.disable_hitbox()
+		hitbox.queue_free()
+	
+	hitboxes.clear()
+	attack_state = AttackState.RECOVERING
+	attacked.emit(attack_skill.stamina_cost, attack_skill.mana_cost)
+	
+	await get_tree().create_timer(attack_skill.recovery).timeout
+	
+	attack_state = AttackState.IDLE
