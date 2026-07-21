43d265d2c7b325aad35cb4950d43a23d208663b2
Toxin4426 <Toxin4426@gmail.com>
2026-07-21 17:01:42 -0400

    update creature vision / senese

--- dungeon-roguelike/Entities/EnemyDebug/vision_component.gd
+++ dungeon-roguelike/Entities/EnemyDebug/vision_component.gd
@@ -8,11 +8,6 @@ signal lost_target(target: Node3D)
 @export var vision_area: Area3D
 @export var ray_cast_3d: RayCast3D
 
-var has_target : bool = false
-var current_target : Node3D
-
-var accetable_targets : Array[Node3D]
-
 func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
 	
@@ -22,34 +17,20 @@ func _on_timer_timeout() -> void:
 		lost_target.emit(null)
 		return
 	
-	var target_in_range : bool = false
 	for overlap in overlaps:
-		if overlap is not Player:
+		if not overlap is Player or overlap is CreatureBase:
 			continue
 		
-		target_in_range = true
 		var local_target : Vector3 = ray_cast_3d.to_local(overlap.global_position)
 		ray_cast_3d.target_position = local_target
 		ray_cast_3d.force_raycast_update()
 		
-		if not ray_cast_3d.is_colliding():
-			lost_target.emit(null)
-			continue
+		#if not ray_cast_3d.is_colliding():
+		#	lost_target.emit(null)
+		#	continue
 		
 		if ray_cast_3d.get_collider() == overlap:
-			if has_target == false:
-				current_target = overlap
-				found_target.emit(current_target)
-				
-			has_target = true
+			found_target.emit(overlap)
 		else:
-			print("LOST")
-			if has_target:
-				lost_target.emit(null)
-				current_target = null
-			has_target = false
-	
-	if not target_in_range:
-		if has_target:
+			print("Raycast Failed to Hit Target")
 			lost_target.emit(null)
-			has_target = false
