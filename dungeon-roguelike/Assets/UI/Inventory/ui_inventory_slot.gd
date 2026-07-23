d0fa8f3f265fefb6cc56e4a933335658a774f487
PranavAlla <Pranavalla@gmail.com>
2026-07-23 15:01:07 -0400

    Saving work on custom drag and drop system

--- dungeon-roguelike/Assets/UI/Inventory/ui_inventory_slot.gd
+++ dungeon-roguelike/Assets/UI/Inventory/ui_inventory_slot.gd
@@ -2,6 +2,10 @@ extends Panel
 
 @onready var item_visual: Sprite2D = $Control/ItemDisplay
 @onready var amount_text: Label = $Control/Label
+#var texture: Texture2D
+
+func process() -> void:
+	pass
 
 func update(slot: InvSlot) -> void:
 	if !slot.item:
@@ -13,3 +17,20 @@ func update(slot: InvSlot) -> void:
 		if slot.amount > 1:
 			amount_text.visible = true
 		amount_text.text = str(slot.amount)
+		#texture = item_visual.texture
+
+func _gui_input(_event: InputEvent) -> void:
+	pass
+
+#func _get_drag_data(_at_position: Vector2) -> Variant:
+	#var wrapper : Control = Control.new()
+	#wrapper.custom_minimum_size = texture.get_size()
+	#
+	#var prev: TextureRect = TextureRect.new()
+	#prev.texture = texture
+	#
+	#wrapper.add_child(prev)
+	#
+	#set_drag_preview(wrapper)
+	#
+	#return self
