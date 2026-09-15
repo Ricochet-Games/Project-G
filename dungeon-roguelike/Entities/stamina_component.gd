83d7ff0e0af5fc6b7f52c07187269d6e34077111
Toxin4426 <Toxin4426@gmail.com>
2026-09-15 11:02:12 -0400

    add stamina regen

--- dungeon-roguelike/Entities/stamina_component.gd
+++ dungeon-roguelike/Entities/stamina_component.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name StaminaComponent
 
-signal used_stamina(amount: int, new_stamina: int)
+signal used_stamina(amount: int)
 # signal gained_stamina(amount: int, new_stamina: int)
 signal stamina_changed(amount: int, new_stamina: int)
 signal out_of_stamina
@@ -10,13 +10,18 @@ signal out_of_stamina
 @export var max_stamina : int = 100
 @export var current_stamina: int = 100
 
+@export var stamina_regen_amount: int = 5
+@onready var stamina_regen_timer: Timer = $StaminaRegenTimer
+@onready var stamina_regen_pause_timer: Timer = $StaminaRegenPauseTimer
+
 @export var attack_component : AttackComponent
 
 
 func _ready() -> void:
 	current_stamina = max_stamina
 	attack_component.started_attack.connect(_on_attack)
-	
+	stamina_regen_timer.timeout.connect(_regenerate_stamina)
+	stamina_regen_pause_timer.timeout.connect(_start_stamina_regen)
 
 func _on_attack(stamina_used: int, _mana_used: int) -> void:
 	if stamina_used == 0:
@@ -25,9 +30,20 @@ func _on_attack(stamina_used: int, _mana_used: int) -> void:
 	if current_stamina <= 0:
 		return
 	
+	stamina_regen_timer.stop()
+	stamina_regen_pause_timer.start()
+	
 	current_stamina = max(current_stamina - stamina_used, 0)
 	used_stamina.emit(stamina_used, current_stamina)
 	stamina_changed.emit(stamina_used, current_stamina)
 	
 	if current_stamina <= 0:
 		out_of_stamina.emit()
+
+func _regenerate_stamina() -> void:
+	if current_stamina < max_stamina:
+		current_stamina = min(current_stamina + stamina_regen_amount, max_stamina)
+		stamina_changed.emit(stamina_regen_amount, current_stamina)
+		
+func _start_stamina_regen() -> void:
+	stamina_regen_timer.start()
