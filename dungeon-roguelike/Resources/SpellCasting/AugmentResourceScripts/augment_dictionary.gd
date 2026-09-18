1a0278c2beaa21356b39e13f2cef411f06974195
PranavAlla <Pranavalla@gmail.com>
2026-09-18 18:38:43 -0400

    added split modifier to dictionary

--- dungeon-roguelike/Resources/SpellCasting/AugmentResourceScripts/augment_dictionary.gd
+++ dungeon-roguelike/Resources/SpellCasting/AugmentResourceScripts/augment_dictionary.gd
@@ -3,5 +3,6 @@ extends Resource
 
 
 var augments: Dictionary = {
-	"swift": preload("res://Resources/SpellCasting/AugmentResourceScripts/augment_data_swift.gd").new()
+	"swift": preload("res://Resources/SpellCasting/AugmentResourceScripts/augment_data_swift.gd").new(),
+	"split": preload("res://Resources/SpellCasting/AugmentResourceScripts/augment_data_split.gd").new()
 }
