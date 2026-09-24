05da8c7da9110c9c270dc7db41133bb5ac9a3c37
Toxin4426 <Toxin4426@gmail.com>
2026-09-24 17:47:05 -0400

    add player state enum

--- dungeon-roguelike/Entities/Player/player.gd
+++ dungeon-roguelike/Entities/Player/player.gd
@@ -16,6 +16,17 @@ class_name Player
 
 @onready var nameplate: Label3D = $Nameplate
 
+@export var state : PlayerState = PlayerState.IDLE 
+	
+enum PlayerState
+{
+	IDLE,
+	WALKING,
+	SPRINTING,
+	ATTACKING,
+}
+
+
 func _enter_tree() -> void:
 	if Network.is_steam_initialized and multiplayer.has_multiplayer_peer():
 		set_multiplayer_authority(int(name))
